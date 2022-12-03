0bb8ad41969eb3f402728254edbfb05fdabde4be
delchiv <ismms@yandex.ru>
2022-12-03 15:38:08 +0300

    В игру добавлен мячик и его движение

--- pingpong.py
+++ pingpong.py
@@ -2,8 +2,16 @@ from pygame import *
 
 window = display.set_mode((700, 500))
 
+ball = image.load("ball.png")
+ball = transform.scale(ball, (30, 30))
+
 clock = time.Clock()
 game = True
+#TODO Слишком много переменных для одного мяча - использовать классы
+ball_x = 50
+ball_y = 50
+ball_speed_x = 3
+ball_speed_y = 3
 while game:
     clock.tick(60)
 
@@ -11,5 +19,20 @@ while game:
         if e.type == QUIT:
             game = False
         
-    window.fill((30, 240, 255))    
+    # Правила
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
+    #TODO Это надо упростить
+    if ball_y+30 >= 500:
+        ball_speed_y *= -1
+    if ball_x+30 >= 700:
+        ball_speed_x *= -1
+    if ball_y < 0:
+        ball_speed_y *= -1
+    if ball_x < 0:
+        ball_speed_x *= -1
+
+    # Отрисовка    
+    window.fill((30, 240, 255))
+    window.blit(ball, (ball_x, ball_y))    
     display.update()
